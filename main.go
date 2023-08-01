0dfd23f11abb6129990566098ff0086344c8489c
edwinsiby <edwinsibyrajakumary@gmail.com>
2023-08-01 19:57:35 +0530

    update-live stream not completed

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net"
-	"time"
+	"net/http"
+	"os"
 
 	pb "live/pb"
 
@@ -17,7 +20,7 @@ type server struct {
 }
 
 func main() {
-	lis, err := net.Listen("tcp", ":8080")
+	lis, err := net.Listen("tcp", ":8081")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
@@ -29,32 +32,74 @@ func main() {
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+
 }
 
 func (s *server) GetLiveStream(stream pb.StreamingService_GetLiveStreamServer) error {
-
 	videoCapture, err := gocv.OpenVideoCapture(0)
 	if err != nil {
 		log.Fatalf("failed to open camera: %v", err)
 	}
 	frame := gocv.NewMat()
 	defer frame.Close()
-
 	for {
 		if ok := videoCapture.Read(&frame); !ok {
 			break
 		}
-
 		data, err := gocv.IMEncode(".jpg", frame)
 		if err != nil {
 			return err
 		}
-
 		if err := stream.Send(&pb.StreamingResponse{Data: data.GetBytes()}); err != nil {
 			return err
 		}
+	}
+	return nil
+}
 
-		time.Sleep(50 * time.Millisecond)
+// func (s *server) GetLiveStream(stream pb.StreamingService_GetLiveStreamServer) error {
+// 	absFilePath, err := filepath.Abs("temp_video.mp4")
+// 	if err != nil {
+// 		log.Fatalf("failed to get absolute file path: %v", err)
+// 	}
+// 	videoURL := "http://localhost:8080/video/streetacademy.mp4"
+// 	if err := downloadVideo(videoURL, absFilePath); err != nil {
+// 		return fmt.Errorf("failed to download video: %v", err)
+// 	}
+// 	defer os.Remove(absFilePath)
+// 	return nil
+// }
+
+func fetchVideoStream(url string) (io.ReadCloser, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("unexpected status code: %s", resp.Status)
+	}
+
+	return resp.Body, nil
+}
+
+func downloadVideo(url, filePath string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("failed to download video: %v", err)
+	}
+	defer resp.Body.Close()
+
+	out, err := os.Create(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to create file: %v", err)
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to save video file: %v", err)
 	}
 
 	return nil
